9c9b6c17c67260a9401193ee071adb98791b8632
David Martinez <dmartinez@bancoconsorcio.cl>
2018-12-17 11:53:55 -0300

    update 0.5.2

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -13,8 +13,8 @@ import { MatSnackBar } from '@angular/material';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor(public planServices: PlanMensualService, 
-              private elementRef: ElementRef, 
+  constructor(public planServices: PlanMensualService,
+              private elementRef: ElementRef,
               private cdRef: ChangeDetectorRef,
               private auth: AuthenticationService,
             public router: Router,
@@ -43,9 +43,9 @@ export class DashboardComponent implements OnInit {
 
   ngOnInit() {
 
-    setTimeout (() => { 
+    setTimeout (() => {
       this.auth.colornav = true;
-  }); 
+  });
 
 
     window.scrollTo(0, 0);
@@ -77,17 +77,17 @@ export class DashboardComponent implements OnInit {
         this.dona = this.planServices.arrayinput.filter(gastos => gastos.sobre == 6)
 
     this.planServices.percent_55 = (this.planServices.sueldo / 100)*55;
-  
-  
+
+
     this.planServices.percent_10 = (this.planServices.sueldo / 100)*10;
-  
+
     this.planServices.percent_5 = (this.planServices.sueldo / 100)*5;
-  
+
 
     this.total_reduce()
 
     this.pieChartResume()
-    
+
     this.BarChartResume()
 
     this.pieChart()
@@ -107,10 +107,10 @@ export class DashboardComponent implements OnInit {
     console.log("wut? ", error)
   })
 
-    
 
 
-    
+
+
 
 }
 
@@ -131,60 +131,60 @@ total_reduce() {
 
 
   var result2 = this.plazo.map(a => a.value);
-  
+
     let total2 = result2.reduce((a, b) => +a + +b, 0);
-  
+
     this.planServices.totalgastos2 = total2;
-  
+
     let resta2 = this.planServices.percent_10 - this.planServices.totalgastos2
-  
+
     this.planServices.salary_less_total_gasto2 = resta2;
-  
+
     var result3 = this.ocio.map(a => a.value);
-  
+
     let total3 = result3.reduce((a, b) => +a + +b, 0);
-  
+
     this.planServices.totalgastos3 = total3;
-  
+
     let resta3 = this.planServices.percent_10 - this.planServices.totalgastos3
-  
+
     this.planServices.salary_less_total_gasto3 = resta3;
-  
+
 
     var result4 = this.educ.map(a => a.value);
-  
+
     let total4 = result4.reduce((a, b) => +a + +b, 0);
-  
+
     this.planServices.totalgastos4 = total4;
-  
+
     let resta4 = this.planServices.percent_10 - this.planServices.totalgastos4
-  
+
     this.planServices.salary_less_total_gasto4 = resta4;
 
 
     var result5 = this.fina.map(a => a.value);
-  
-    let total5= result5.reduce((a, b) => +a + +b, 0);
-  
-    this.planServices.totalgastos5 = total5;
-  
-    let resta5 = this.planServices.percent_10 - this.planServices.totalgastos5
-  
-    this.planServices.salary_less_total_gasto5 = resta5;
-  
 
-    
+    let total5= result5.reduce((a, b) => +a + +b, 0);
+
+    this.planServices.totalgastos5 = total5;
+
+    let resta5 = this.planServices.percent_10 - this.planServices.totalgastos5
+
+    this.planServices.salary_less_total_gasto5 = resta5;
+
+
+
       var result6 = this.dona.map(a => a.value);
-    
+
       let total6= result6.reduce((a, b) => +a + +b, 0);
-    
+
 
       this.planServices.totalgastos6 = total6;
-    
+
       let resta6 = this.planServices.percent_5 - this.planServices.totalgastos6
-    
+
       this.planServices.salary_less_total_gasto6 = resta6;
-    
+
 
       this.suma_total_gastos = this.planServices.totalgastos1 + this.planServices.totalgastos2 + this.planServices.totalgastos3
                                 + this.planServices.totalgastos4 + this.planServices.totalgastos5 + this.planServices.totalgastos6
@@ -195,13 +195,13 @@ total_reduce() {
 
 
 ngAfterViewInit() {
-  setTimeout (() => { 
-   
+  setTimeout (() => {
+
         this.auth.colornav = true;
       this.cdRef.detectChanges();
 
-    }); 
-  
+    });
+
   }
 
 
@@ -226,7 +226,7 @@ ngAfterViewInit() {
       this.openSnackBar("Con poco dinero que donen muchas personas a la vez se cambia el mundo (o por lo menos el mundo de una persona) y, además, esa pequeña donación ayuda a mejorar de alguna manera el sistema en el que vivimos.", "cerrar");
     }
 
- 
+
   }
 
 
@@ -248,37 +248,37 @@ adminCard(number){
   console.log(number)
 
   if (number == 1){
-    
+
         this.planServices.card1 = true;
-    
+
       }
       if (number == 2){
-        
+
             this.planServices.card2 = true;
-        
+
           }
           if (number == 3){
-            
+
                 this.planServices.card3 = true;
-            
+
               }
               if (number == 4){
-                
+
                     this.planServices.card4 = true;
-                
+
                   }
                   if (number == 5){
-                    
+
                         this.planServices.card5 = true;
-                    
+
                       }
 
                       if (number == 6){
-                        
+
                             this.planServices.card6 = true;
-                        
+
                           }
-  
+
                           this.router.navigate(['panel'])
 }
 
@@ -328,29 +328,34 @@ pieChart() {
 
       }
       });
-    
+
     }
 
     pieChartResume() {
 
-      
+
         const formatter = new Intl.NumberFormat('de-DE', {
-      
+
           currency: 'USD',
           minimumFractionDigits: 0
         })
-      
-        let x = formatter.format(this.suma_total_gastos)
-      
-        let z = formatter.format(this.suma_total_disponible)
-      
-      
-      
-        this.planServices.total_gasto = x;
 
-        this.planServices.total_disponible = z;
 
-        
+        if(this.suma_total_gastos >= 1000000 ){
+
+          const x = this.suma_total_gastos
+          const z = this.suma_total_disponible
+        }
+        else {
+
+          const x = formatter.format(this.suma_total_gastos)
+
+          const z = formatter.format(this.suma_total_disponible)
+
+
+        }
+
+
 
             let htmlRef = this.elementRef.nativeElement.querySelector(`#pieChartResume`);
             this.myChart = new Chart(htmlRef, {
@@ -359,33 +364,33 @@ pieChart() {
                   labels: ["Total Gastado", "Total Disponible",],
                   datasets: [{
                       label: '',
-                      data: [this.planServices.total_gasto ,  this.planServices.total_disponible],
+                      data: [x,  z],
                       backgroundColor: [
                           '#e0cade',
                           '#7cd8a0'
                       ],
-      
+
                       borderWidth: 1
                   }]
               },
               options: {
                 responsive: false,
-      
+
             }
             });
-          
+
           }
 
 
           BarChartResume() {
-            
+
 
                     const formatter = new Intl.NumberFormat('de-DE', {
-                  
+
                       currency: 'USD',
                       minimumFractionDigits: 0
                     })
-                  
+
 
                     let x1 = formatter.format(this.planServices.totalgastos1)
 
@@ -399,8 +404,8 @@ pieChart() {
 
                     let x6 = formatter.format(this.planServices.totalgastos6)
 
-                  
-                    
+
+
                     this.gastos1 = x1;
                     this.gastos2 = x2;
                     this.gastos3 = x3;
@@ -415,61 +420,61 @@ pieChart() {
                     let z5 = formatter.format(this.planServices.salary_less_total_gasto5)
                     let z6 = formatter.format(this.planServices.salary_less_total_gasto6)
 
-                    
+
                     var gastadoData = {
                       label: 'Total Gastado',
                       data: [this.gastos1, this.gastos2, this.gastos3, this.gastos4, this.gastos5, this.gastos6],
-                      backgroundColor: '#e0cade',                                            
+                      backgroundColor: '#e0cade',
 
-                      
-                      
-                    }; 
+
+
+                    };
 
                     var disponibleData = {
                       label: 'Total Disponible',
                       data: [z1, z2, z3, z4, z5, z6],
                       backgroundColor: '#7cd8a0'
 
-                     
+
                     };
 
                     var modulosData = {
                       labels: ["Gastos fijos", "Ahorro", "Ocio", "Educacion", "Finanzas", "Donaciones"],
-                      datasets: [gastadoData, disponibleData]               
+                      datasets: [gastadoData, disponibleData]
                     };
 
                     let htmlRef = this.elementRef.nativeElement.querySelector(`#BarChartResume`);
                     this.myChart = new Chart(htmlRef, {
                       type: 'horizontalBar',
                       data: modulosData,
-                      
+
                     });
-                  
-                      
+
+
                       }
 
     pieChart2() {
-      
+
         const formatter = new Intl.NumberFormat('de-DE', {
-      
+
           currency: 'USD',
           minimumFractionDigits: 0
         })
-      
+
         let x = formatter.format(this.planServices.totalgastos2)
-      
+
         let z = formatter.format(this.planServices.salary_less_total_gasto2)
-      
-      
+
+
         this.planServices.totalgastos2 = x;
-      
+
         this.planServices.salary_less_total_gasto2 = z;
-      
+
         console.log("formated 2 : ", x)
-      
-        
-                
-      
+
+
+
+
             let htmlRef = this.elementRef.nativeElement.querySelector(`#myChart2`);
             this.myChart = new Chart(htmlRef, {
               type: 'doughnut',
@@ -482,43 +487,43 @@ pieChart() {
                           '#e0cade',
                           '#7cd8a0'
                       ],
-      
+
                       borderWidth: 1
                   }]
               },
               options: {
                 responsive: false,
-      
+
             }
             });
-          
+
           }
-    
+
 
 
 
           pieChart3() {
-            
+
               const formatter = new Intl.NumberFormat('de-DE', {
-            
+
                 currency: 'USD',
                 minimumFractionDigits: 0
               })
-            
+
               let x = formatter.format(this.planServices.totalgastos3)
-            
+
               let z = formatter.format(this.planServices.salary_less_total_gasto3)
-            
-            
+
+
               this.planServices.totalgastos3 = x;
-            
+
               this.planServices.salary_less_total_gasto3 = z;
-            
+
               console.log("formated: ", x)
-            
-              
-                      
-            
+
+
+
+
                   let htmlRef = this.elementRef.nativeElement.querySelector(`#myChart3`);
                   this.myChart = new Chart(htmlRef, {
                     type: 'doughnut',
@@ -531,42 +536,42 @@ pieChart() {
                                 '#e0cade',
                                 '#7cd8a0'
                             ],
-            
+
                             borderWidth: 1
                         }]
                     },
                     options: {
                       responsive: false,
-            
+
                   }
                   });
-                
-                }     
+
+                }
 
 
 
                 pieChart4() {
-                  
+
                     const formatter = new Intl.NumberFormat('de-DE', {
-                  
+
                       currency: 'USD',
                       minimumFractionDigits: 0
                     })
-                  
+
                     let x = formatter.format(this.planServices.totalgastos4)
-                  
+
                     let z = formatter.format(this.planServices.salary_less_total_gasto4)
-                  
-                  
+
+
                     this.planServices.totalgastos4 = x;
-                  
+
                     this.planServices.salary_less_total_gasto4 = z;
-                  
+
                     console.log("formated: ", x)
-                  
-                    
-                            
-                  
+
+
+
+
                         let htmlRef = this.elementRef.nativeElement.querySelector(`#myChart4`);
                         this.myChart = new Chart(htmlRef, {
                           type: 'doughnut',
@@ -579,41 +584,41 @@ pieChart() {
                                       '#e0cade',
                                       '#7cd8a0'
                                   ],
-                  
+
                                   borderWidth: 1
                               }]
                           },
                           options: {
                             responsive: false,
-                  
+
                         }
                         });
-                      
+
                       }
 
 
                       pieChart5() {
-                        
+
                           const formatter = new Intl.NumberFormat('de-DE', {
-                        
+
                             currency: 'USD',
                             minimumFractionDigits: 0
                           })
-                        
+
                           let x = formatter.format(this.planServices.totalgastos5)
-                        
+
                           let z = formatter.format(this.planServices.salary_less_total_gasto5)
-                        
-                        
+
+
                           this.planServices.totalgastos5 = x;
-                        
+
                           this.planServices.salary_less_total_gasto5 = z;
-                        
+
                           console.log("formated: ", x)
-                        
-                          
-                                  
-                        
+
+
+
+
                               let htmlRef = this.elementRef.nativeElement.querySelector(`#myChart5`);
                               this.myChart = new Chart(htmlRef, {
                                 type: 'doughnut',
@@ -626,40 +631,40 @@ pieChart() {
                                             '#e0cade',
                                             '#7cd8a0'
                                         ],
-                        
+
                                         borderWidth: 1
                                     }]
                                 },
                                 options: {
                                   responsive: false,
-                        
+
                               }
                               });
-                            
+
                             }
-                      
+
                             pieChart6() {
-                              
+
                                 const formatter = new Intl.NumberFormat('de-DE', {
-                              
+
                                   currency: 'USD',
                                   minimumFractionDigits: 0
                                 })
-                              
+
                                 let x = formatter.format(this.planServices.totalgastos6)
-                              
+
                                 let z = formatter.format(this.planServices.salary_less_total_gasto6)
-                              
-                              
+
+
                                 this.planServices.totalgastos6 = x;
-                              
+
                                 this.planServices.salary_less_total_gasto6 = z;
-                              
+
                                 console.log("formated: ", x)
-                              
-                                
-                                        
-                              
+
+
+
+
                                     let htmlRef = this.elementRef.nativeElement.querySelector(`#myChart6`);
                                     this.myChart = new Chart(htmlRef, {
                                       type: 'doughnut',
@@ -672,18 +677,17 @@ pieChart() {
                                                   '#e0cade',
                                                   '#7cd8a0'
                                               ],
-                              
+
                                               borderWidth: 1
                                           }]
                                       },
                                       options: {
                                         responsive: false,
-                              
+
                                     }
                                     });
-                                  
-                                  }
-                            
-                
-}
 
+                                  }
+
+
+}
